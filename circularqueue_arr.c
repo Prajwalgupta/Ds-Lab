fcb6a521956a8080da1af87e4a1b4f352088a546
Prajwal Gupta <92562327+Prajwalgupta@users.noreply.github.com>
2023-02-12 03:50:28 +0530

    Create circularqueue_arr.c

---
+++ circularqueue_arr.c
@@ -0,0 +1,90 @@
+#include <stdio.h>  
+  
+# define max 8  
+int queue[max],front=-1,rear=-1;  // array declaration  ; 
+
+
+// function to insert an element in a circular queue  
+void enqueue(int element)  
+{  
+    if(front==-1 && rear==-1)   // condition to check queue is empty  
+    {  
+        front=0;  
+        rear=0;  
+        queue[rear]=element;  
+    }  
+    else if((rear+1)%max==front)  // condition to check queue is full  
+    {  
+        printf("Queue is overflow..");  
+    }  
+    else  
+    {  
+        rear=(rear+1)%max;       // rear is incremented  
+        queue[rear]=element;     // assigning a value to the queue at the rear position.  
+    }  
+}  
+  
+// function to delete the element from the queue  
+int dequeue()  
+{  
+    if((front==-1) && (rear==-1))  // condition to check queue is empty  
+    {  
+        printf("\nQueue is underflow..");  
+    }  
+ else if(front==rear)  
+{  
+   printf("\nThe dequeued element is %d", queue[front]);  
+   front=-1;  
+   rear=-1;  
+}   
+else  
+{  
+    printf("\nThe dequeued element is %d", queue[front]);  
+   front=(front+1)%max;  
+}  
+}  
+// function to display the elements of a queue  
+void display()  
+{  
+    int i=front;  
+    if(front==-1 && rear==-1)  
+    {  
+        printf("\n Queue is empty..");  
+    }  
+    else  
+    {  
+        printf("\nElements in a Queue are :\n");  
+        while(i<=rear)  
+        {  
+            printf("%d,", queue[i]);  
+            i=(i+1)%max;  
+        }  
+    }  
+}  
+int main()  
+{  
+    int choice=1,x;   // variables declaration  
+      
+    while(choice<4 && choice!=0)   // while loop  
+    {  
+    printf("\nSelect Operation From below menu\n");  
+    printf("===============***===============\n");  
+    printf("1.Insertion\n2.Deletion\n3.Display\n");  
+    printf("Enter your choice:\n");  
+    scanf("%d",&choice);
+    switch(choice)  
+    {  
+        case 1:printf("Enter the element which is to be inserted:\n");  
+        scanf("%d", &x);  
+        enqueue(x);break;
+        case 2:  
+        dequeue();  
+        break;  
+        case 3:  
+        display();break;
+        default:printf("Invalid choice\n");
+  
+    }
+    }  
+    return 0;  
+}  
